efd393d1bd76204e0c45a7fd921490962f272ab8
Abdullah darwesh <abdaladarwesh63@gmail.com>
2025-08-03 12:57:51 +0300

    Adding a top bar and fix the side bar

--- src/Components/Sidebar.tsx
+++ src/Components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import type { FC } from "react";
+import { useState, type FC } from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.png"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -7,23 +7,41 @@ import { faChartLine , faGear, faArrowRightFromBracket} from "@fortawesome/free-
 
 
 const Sidebar : FC = () => {
+    const [selectedIcon, setSelectedIcon] = useState<number>(1);
+
     return(
 
-        <div className="flex flex-col bg-gray-700 w-15 ml-2 mt-2 mb-2 rounded-xl shadow-2xl items-center">
-            <div className="size-[50px] bg-gray-950 mt-2 rounded-xl flex justify-center items-center">
-                <Link to={"/"}>
-                    <img src={logo} alt="Urvibes logo" width={40} height={40}/>
+        <div className="flex flex-col bg-gray-700 w-15 ml-2 mt-2 mb-2 rounded-xl shadow-2xl items-center z-50">
+            <div className="size-[50px] bg-gray-950 mt-2 rounded-xl flex justify-center items-center cursor-pointer">
+                <Link to={"/"} className="cursor-pointer">
+                    <img src={logo} alt="Urvibes logo" width={40} height={40} className="cursor-pointer"/>
                 </Link>
             </div>
             <div className="flex flex-col mt-10 gap-8">
-                <Link to={"/"}>
-                    <FontAwesomeIcon icon={faHouse} className="text-white" cursor={"pointer"}/>
-                </Link>
-                <FontAwesomeIcon icon={faChartLine} className="text-white" cursor={"pointer"}/>
-                <FontAwesomeIcon icon={faCalendarDays} className="text-white" cursor={"pointer"}/>
-                <FontAwesomeIcon icon={faGear} className="text-white" cursor={"pointer"}/>
+                {
+                    [
+                        {id : 1, icon : faHouse},
+                        {id : 2, icon : faChartLine},
+                        {id : 3, icon : faCalendarDays},
+                        {id : 4, icon : faGear}
+                    ].map(({id, icon}) => {
+                            return (
+                                <div className="relative flex items-center" key={id}>
+                                    {
+                                        selectedIcon === id && 
+                                        <div className="absolute -left-5 w-1 h-6 bg-white rounded-tr-full rounded-br-full"></div>
+                                    }
+                                    <Link to={"/"} onClick={() => setSelectedIcon(id)}>
+                                        <FontAwesomeIcon icon={icon} className="text-white cursor-pointer"/>
+                                    </Link>
+                                </div>
+                            ); 
+                    })
+                }
             </div>
-            <FontAwesomeIcon icon={faArrowRightFromBracket} className="text-white mt-auto mb-5" cursor={"pointer"}/>
+            <Link to={"/"} className="text-white mt-auto mb-5">
+                <FontAwesomeIcon icon={faArrowRightFromBracket} cursor={"pointer"}/>
+            </Link>
         </div>
     );
 }
--- src/Components/Topbar.tsx
+++ src/Components/Topbar.tsx
@@ -1,12 +1,54 @@
-import type { FC } from "react";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faBell} from "@fortawesome/free-regular-svg-icons"
 
-
-const Topbar : FC = () => {
-    return(
-        <div className="flex flex-row mt-5 ml-5">
-            <h1 className="text-3xl text-white font-outfit">Hello Sir !</h1>
-        </div>
-    );
+type props = {
+  img? : string
 }
+
+const Topbar = ({img} : props) => {
+
+  return (
+    <div className="flex fixed top-0 w-full justify-end items-center mt-7 px-7 gap-6 z-20">
+      <h1 className="text-2xl text-white mr-auto ml-20 font-outfit whitespace-nowrap">
+        Hello Sir!
+      </h1>
+
+      <div className="relative w-75">
+        <input
+          type="text"
+          placeholder="Search..."
+          aria-label="Search"
+          className="w-full pl-10 pr-4 py-2 rounded-4xl bg-gray-700/90 text-white placeholder-gray-500 focus:outline-none placeholder:text-sm"
+        />
+        <span className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-500 pointer-events-none">
+          <FontAwesomeIcon icon={faMagnifyingGlass} />
+        </span>
+      </div>
+      <div className="rounded-full bg-gray-700/90 size-10 flex items-center justify-center">
+        <FontAwesomeIcon icon={faBell} className="text-white"/>
+      </div>
+      {
+      img == null ? 
+          <img 
+          src="https://eu.ui-avatars.com/api/?name=Abdullah+Darwesh&size=250&color=191a1a"
+          className="rounded-full bg-gray-700/90 size-10 flex items-center justify-center"
+          alt="Profile picture"
+          ></img> 
+      : 
+          <img 
+          src={img}
+          className="rounded-full bg-gray-700/90 size-10 flex items-center justify-center"
+          alt="Profile picture"
+          >
+          </img>
+      }
+      
+        
+    </div>
+  );
+}
+
+
 
 export default Topbar;
